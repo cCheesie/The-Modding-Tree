11153642729b14f07df61a1921a5d4b5f817f7d7
cCheesie <Cheesieweasles@gmail.com>
2024-03-09 16:30:10 +0000

    Update mod.js
    Started the game

--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
+	name: "The Cheesie Tree",
+	id: "thesecretcheese",
 	author: "nobody",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
